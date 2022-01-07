ee62299dc3f0ac7751eb2da19a9bfefc8465017b
Samalairbien <74256932+Samalairbien@users.noreply.github.com>
2022-01-07 11:46:33 +0100

    Update Facture.java

--- src/metier/Facture.java
+++ src/metier/Facture.java
@@ -2,63 +2,71 @@ package metier;
 
 import java.time.LocalDate;
 
-public class Facture
+public class Facture 
 {
-	/**
-	 * Retourne le client à qui est adressée la facture..
-	 * @return le client.
-	 */
-	
-	public Client getClient()
-	{
-		return null;
-	}
+    Client client;
+    private int montantFacture;
+    private LocalDate dateFacture;
+    boolean reglementFacture;
 
-	/**
-	 * Retourne le montant de la facture.
-	 * @return le montant de la facture.
-	 */
-	
-	public int getMontant()
-	{
-		return 0;
-	}
+    public Facture(Client client, int montant, boolean reglementFacture, LocalDate date) 
+    {
+        this.client = client;
+        montantFacture = montant;
+        dateFacture = LocalDate.now();
+        reglementFacture = false;
+    }
 
-	/**
-	 * Retourne vrai si la facture est reglée.
-	 * @return vrai ssi la facture est reglée.
-	 */
-	
-	public boolean estReglee()
-	{
-		return true;
-	}
 
-	/**
-	 * Retourne la date de la facture.
-	 * @return la date de la facture.
-	 */
-	
-	public LocalDate getDate()
-	{
-		return null;
-	}
+    public Client getClient() {
+        return client;
+    }
 
-	/**
-	 * Supprime la facture
-	 */
-	
-	public void delete()
-	{
-	}
-	
-	/**
-	 * Duplique la facture.
-	 * @return une copie de la facture.
-	 */
-	
-	public Facture copie()
-	{
-		return null;
-	}
+    /**
+     * Retourne le montant de la facture.
+     * @return le montant de la facture.
+     */
+
+    public int getMontant() {
+        return montantFacture;
+    }
+
+    /**
+     * Retourne vrai si la facture est reglée.
+     * @return vrai si la facture est reglée.
+     */
+
+    public boolean estReglee() 
+    {
+        return reglementFacture;
+    }
+
+    /**
+     * Retourne la date de la facture.
+     * @return la date de la facture.
+     */
+
+    public LocalDate getDate() 
+    {
+        return dateFacture;
+    }
+
+    /**
+     * Supprime la facture
+     */
+
+    public void delete() 
+    {
+        getClient().getFactures().remove(this);
+    }
+
+    /**
+     * Duplique la facture.
+     * @return une copie de la facture.
+     */
+
+    public Facture copie() 
+    {
+        return getClient().createFacture(getMontant(), estReglee());
+    }
 }
