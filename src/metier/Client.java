ff459bab3a671508279a000201ebe6ba52f36536
Samalairbien <74256932+Samalairbien@users.noreply.github.com>
2022-01-07 11:56:13 +0100

    Update Client.java

--- src/metier/Client.java
+++ src/metier/Client.java
@@ -1,49 +1,63 @@
 package metier;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Client
 {
+	private String nom;
+	private List<Facture> factures = new ArrayList<Facture>();
+	private static List<Client> clients = new ArrayList<Client>();
 	/** 
 	 * Crée un client.
 	 * @param nom le nom du client. 
 	 */
-	
+
 	public Client(String nom)
 	{
+		this.nom = nom;
+		clients.add(this);
 	}
 
 	/**
 	 * Retourne le nom du client.
 	 * @return le nom du client.
 	 */
-	
+
 	public String getNom()
 	{
-		return null;
+		return nom;
 	}
-	
+
 	/**
 	 * Modifie le nom du client.
 	 * @param nom le nom du client.
 	 */
-	
+
 	public void setNom(String nom)
 	{
+		this.nom = nom;
 	}
-	
+
 	/**
 	 * Créé une facture.
 	 * @param montant Le montant de la facture.
 	 * @return la facture créée.
 	 */
-	
+
 	public Facture createFacture(int montant)
 	{
-		return null;
+		if (montant < 0) {
+			return null;
+		}
+		else {
+			Facture facture = new Facture(this, montant, false, LocalDate.now());
+			factures.add(facture);
+			return facture;
+		}
 	}
-	
+
 	/**
 	 * Retourne une copie de la liste des factures du client. 
 	 * @return une copie de la liste des factures du client.
@@ -51,31 +65,45 @@ public class Client
 
 	public List<Facture> getFactures()
 	{
-		return null;
+		List<Facture> Facture = new ArrayList<Facture>(factures);
+		return Facture;
 	}
-	
+
 	/**
 	 * Retourne la somme des montants des factures.
 	 * @return la somme des montants des factures.
 	 */
-	
+
 	public int sommeMontants()
 	{
-		return 0;
+		int montant=0;
+		for(Facture facture : factures)
+		{
+			montant += montant+facture.getMontant();
+		}
+		return montant;
 	}
 
 	/**
 	 * Créé une facture en précisant si elle est reglée.
-	 * @param montant Le montant de la facture.
-	 * @param reglée Vrai si la facture est reglée.
+	 * param montant Le montant de la facture.
+	 * param estreglee() Vrai si la facture est reglée.
 	 * @return la facture créée.
 	 */
-	
-	public Facture createFacture(int montant, boolean reglee)
+
+	public Facture createFacture(int montant, boolean reglementFacture)
 	{
-		return null;
-	}	
-	
+		if (montant < 0) {
+			System.out.println("Le montant d'une facture ne peut pas être négatif.");
+			return null;
+		}
+		else {
+			Facture facture = new Facture(this, montant, reglementFacture, LocalDate.now());
+			factures.add(facture);
+			return facture;
+		}
+	}    
+
 	/**
 	 * Retourne la liste des factures reglées. 
 	 * @return la liste des factures reglées.
@@ -83,9 +111,17 @@ public class Client
 
 	public List<Facture> facturesReglees()
 	{
-		return null;
+
+		List<Facture> factureReglees = new ArrayList<Facture>();
+		for(Facture facture : factures)
+		{
+			if(facture.estReglee())
+			{
+				factureReglees.add(facture);
+			}
+		}
+		return factureReglees;
 	}
-	
 
 	/**
 	 * Retourne tous les clients créés.
@@ -93,14 +129,19 @@ public class Client
 	 */
 	public static List<Client> tous()
 	{
-		return null;
+		return new ArrayList<Client>(clients);
 	}
-	
+
 	/**
 	 * Supprime le client.
 	 */
-	
+
 	public void delete()
 	{
+		clients.remove(this);
+	}
+
+	public void deleteFacture(Facture facture) {
+		factures.remove(facture);
 	}
 }
